b7c6b391d7f944e4fe85690117d007fab6373bea
Kipling Gillespie <kiplinggillespie@gmail.com>
2017-10-20 13:42:48 -0400

    Update and rename Testing.py to Large Prime Generator.py
    Added comments

--- Large Prime Generator.py
+++ Large Prime Generator.py
@@ -2,6 +2,7 @@ import math
 import random
 import time
 
+#Modular Exponentiation
 def modexp(integer, exponant, N):
     if exponant == 0:
         return 1
@@ -10,19 +11,21 @@ def modexp(integer, exponant, N):
         return (z*z)%N
     else:
         return (integer*z*z)%N
-
+    
+#Greatest Common Divisor using Euclidian Algorithm
 def gcd(a, b):
     if b == 0:
         return a
     return gcd(b, a%b)
 
-
+#Greatest Common Divisor using exteded Euclidian Algorithm
 def egcd(a, b):
     if b == 0: 
         return (1, 0, a)
     xp, yp, d = egcd(b, a%b)
     return (yp, xp - (math.floor(a/b)*yp), d)
 
+#Test Primality of p using Fermat's Little Theorem
 def primality(p, accuracy = 10):
     
     if (not p&1):
@@ -39,6 +42,7 @@ def primality(p, accuracy = 10):
             return False
     return True
 
+#Generate a prime number that has 64 bits.
 def genprime(bindigits = 64):
     largest = (2**bindigits)-1
     smallest = (2**(bindigits-1))
@@ -55,9 +59,12 @@ def genprime(bindigits = 64):
     print("Found prime after", tries, "tries.")
     return a 
 
+# PiFunc calculates how many prime numbers there are less than x
 def PiFunc(x):
     return x/math.log(x)
 
+#ProbPrime is supposed to calculate the chance of randomly selecting a prime
+# number from a range of integers. It doesn't really work yet.
 def ProbPrime(binBits):
     largest = (2**binBits)-1
     smallest = (2**(binBits-1))
